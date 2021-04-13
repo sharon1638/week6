394d8adadfc57edda3f77a51768a33aa9f8ee0ae
siyun <sharon1638@naver.com>
2021-04-13 17:28:02 +0900

    secondcommit

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+def showmain(request):
+    return render(request, 'main/mainpage.html')
+
+def childhood(request):
+    return render(request, 'main/childhood.html')
+
+def teenager(request):
+    return render(request, 'main/teenager.html')
+
+def adult(request):
+    return render(request, 'main/adult.html')
+# Create your views here.
