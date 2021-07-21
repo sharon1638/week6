f597beaed754e162a3f70188361b1c34114cbaa9
Siyun Kim <sharon1638@naver.com>
2021-07-21 19:41:10 +0900

    Add requirements

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect, render,get_object_or_404
-from .models import Post
+from django.shortcuts import redirect, render,get_object_or_404, redirect
+from .models import Post, Comment
 from django.utils import timezone
 
 def showmain(request):
@@ -20,8 +20,9 @@ def posts(request):
     return render(request, 'main/posts.html',{'posts':posts})
 
 def detail(request,id):
-    post = get_object_or_404(Post, pk=id)
-    return render(request, 'main/detail.html',{'post':post})
+    post=get_object_or_404(Post, pk =id)
+    all_comments=post.comments.all().order_by('-created_at')
+    return render(request,'main/detail.html',{'post':post, 'comments':all_comments})
 
 def new(request):
     return render(request, 'main/new.html')
@@ -55,3 +56,14 @@ def delete(request, id):
     delete_post = Post.objects.get(id = id)
     delete_post.delete()
     return redirect('main:posts')
+
+def create_comment(request, post_id):
+    if request.method == "POST":
+        post = get_object_or_404(Post, pk=post_id)
+        current_user=request.user
+        comment_content=request.POST.get('content')
+        Comment.objects.create(content=comment_content, writer=current_user, post=post)
+    return redirect('main:detail', post_id)
+
+
+    
